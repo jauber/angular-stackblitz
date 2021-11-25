b69863d24f9e007327f00375db88bb7f37a0e328
Jauber.LUA <jauber@gmail.com>
2021-11-25 14:00:37 -0300

    Acrescentando form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+//Se der pau no form mudar para antes do CartService esta linha abaixo
+//no passo a passo ele colocou antes, mas resolvi mudar para testar se a posicao
+//influencia.
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,9 +14,22 @@ export class CartComponent implements OnInit {
 
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  })
+
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {}
+
+  onSubmit(): void {
+    //Processa os dados do checkout
+    this.items = this.cartService.clearCart();
+    console.warn('Seus pedidos foram enviados', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
   ngOnInit(): void {
   }
